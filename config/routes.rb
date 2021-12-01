0fb8a5a8622af798e5113476c16857e2d14e3b86
Lukas Leys <lukas.leys.2@gmail.com>
2021-12-01 18:35:59 +0000

    quotes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/howitworks', to: 'pages#howitworks'
   resources :preferences, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
