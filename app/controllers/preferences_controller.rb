aa3b12344af0939ab4e25810913836adc81458c9
Tiago Collot <collot.tiago1@gmail.com>
2021-12-01 17:31:42 +0000

    Preferences/Show/Css

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,22 +3,25 @@ class PreferencesController < ApplicationController
   end
 
   def new
-
     @event_type = ["Home", "Business", "Special Ocasion"]
     @daytime = ["Day", "Night"]
-    @cocktail_category = ["Fruity", "Spicy", "Salty",  "Sour"]
+    @cocktail_category = ["Fruity", "Spicy", "Salty", "Sour"]
     @preference = Preference.new
   end
 
   def create
     @preference = Preference.new(preference_params)
     @preference.save
-    end
+  end
+
+  def show
+    @preference = Preference.find(params[:id])
+    @recipes = Recipe.all.slice(0..3)
+  end
 
   private
 
   def preference_params
     params.require(:preference).permit(:event_type, :daytime, :cocktail_category)
   end
-
 end
