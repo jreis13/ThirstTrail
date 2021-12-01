f071d99dc4a7bdad0d9372c67c05a53a1b522f3f
Joao Reis <joaofranciscoreis13@outlook.com>
2021-12-01 11:18:16 +0000

    done for now

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,7 +3,6 @@ class PreferencesController < ApplicationController
   end
 
   def new
-
     @event_type = ["Home", "Business", "Special Ocasion"]
     @daytime = ["Day", "Night"]
     @cocktail_category = ["Fruity", "Spicy", "Salty",  "Sour"]
@@ -13,12 +12,11 @@ class PreferencesController < ApplicationController
   def create
     @preference = Preference.new(preference_params)
     @preference.save
-    end
+  end
 
   private
 
   def preference_params
     params.require(:preference).permit(:event_type, :daytime, :cocktail_category)
   end
-
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,5 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
 end
