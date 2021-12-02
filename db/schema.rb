be38f9878ccf91955e3651f8f0a81ba3acd567e6
Joao Reis <joaofranciscoreis13@outlook.com>
2021-12-02 10:43:34 +0000

    small fixes to homepage

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_143453) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
