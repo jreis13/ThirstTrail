f3e9c134dd1261d7815dd82fcc92d6ad5c0137f4
Lukas Leys <lukas.leys.2@gmail.com>
2021-12-03 10:20:58 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/aboutus', to: 'pages#aboutus'
   get '/contact', to: 'pages#contact'
   get '/ourteam', to: 'pages#ourteam'
+  get '/partners', to: 'partners#ourpartners'
   resources :preferences, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_103350) do
+ActiveRecord::Schema.define(version: 2021_12_02_151125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,11 @@ ActiveRecord::Schema.define(version: 2021_12_02_103350) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "our_partners", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
