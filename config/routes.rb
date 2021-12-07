91ea3db0cc953e48d1efcfbb433c9819f6e45409
Lukas Leys <lukas.leys.2@gmail.com>
2021-12-07 10:03:24 +0000

    partners page done

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   get '/ourteam', to: 'pages#ourteam'
   get '/getstarted', to: 'pages#getstarted'
+  get '/partners', to: 'pages#partners'
   resources :preferences, only: [:index, :new, :create, :show]
   resources :recipes, only: [:index, :show]
 
