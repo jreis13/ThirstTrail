fb8099083a65728cf87986fa5c243a8f13d25479
Joao Reis <joaofranciscoreis13@outlook.com>
2021-12-09 11:35:32 +0000

    in progress

--- app/javascript/components/roulette.js
+++ app/javascript/components/roulette.js
@@ -8,25 +8,25 @@ const roulette = () => {
 
   const button = document.querySelector(".spin");
   const randomButton = document.querySelector('.random-recipe')
-  button.addEventListener("click", () => {
+  if (button) {
+    button.addEventListener("click", () => {
     //$(".wheel img").css("filter", "blur(8px)");
 
-    var spininterval = getRandomInt(0, 37) * (360 / 37) + getRandomInt(3, 4) * 360;
-    currentLength += spininterval;
+      var spininterval = getRandomInt(0, 37) * (360 / 37) + getRandomInt(3, 4) * 360;
+      currentLength += spininterval;
 
-    var numofsecs = spininterval;
+      var numofsecs = spininterval;
 
-    $(".wheel img").css("transform", "rotate(" + currentLength + "deg)");
+      $(".wheel img").css("transform", "rotate(" + currentLength + "deg)");
 
-    setTimeout(function () {
-      $(".wheel img").css("filter", "blur(0px)");
-      randomButton.classList.remove('d-none');
-    }, numofsecs);
-  });
-
+      setTimeout(function () {
+        $(".wheel img").css("filter", "blur(0px)");
+        randomButton.classList.remove('d-none');
+      }, numofsecs);
+    });
+  }
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
 };
 export { roulette }
