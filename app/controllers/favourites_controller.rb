1ccc11f88faaee642451e2a1e179a8c466695a9a
Tiago Collot <collot.tiago1@gmail.com>
2021-12-09 11:56:48 +0000

    Like/Unlike favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,10 +2,14 @@ class FavouritesController < ApplicationController
   def favorite
     @recipe = Recipe.find(params[:id])
     current_user.favorited?(@recipe) ? current_user.unfavorite(@recipe) : current_user.favorite(@recipe)
-    redirect_to recipe_path(@recipe)
+    if action_name == "show"
+      redirect_to recipe_path(@recipe)
+    else
+      redirect_to selection_recipes_path
+    end
   end
 
   def selection
-    @favorites = Favorite.where(favoritor: current_user)
+    @favorites = current_user.all_favorites
   end
 end
